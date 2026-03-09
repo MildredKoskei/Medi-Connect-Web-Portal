810aba5d13e9415228feebe0fde11603a153e456
MildredKoskei <mildredkoskei01@gmail.com>
2026-03-09 21:24:11 +0000

    installing flask, creating sqlite database

---
+++ db.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+print("starting script")
+
+conn = sqlite3.connect("database.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    password TEXT,
+    role TEXT
+)
+""")
+
+conn.commit()
+conn.close()
+
+print("Database created successfully")
